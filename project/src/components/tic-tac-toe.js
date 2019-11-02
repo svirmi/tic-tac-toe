62ab6f657bd545cfe81221162c78f3155fcb9605
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-11-02 15:36:23 +0200

    making functional component out of class-based component

--- project/src/components/tic-tac-toe.js
+++ project/src/components/tic-tac-toe.js
@@ -1,17 +1,11 @@
 import React from "react";
 
-class Square extends React.Component {
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={ () => this.props.onClick() }
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
